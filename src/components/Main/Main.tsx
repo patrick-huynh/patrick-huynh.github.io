a48476278c76cb0e4d861000b0e151be22a3f523
Patrick Huynh <patrickhuynh2002@gmail.com>
2022-10-05 17:45:48 -0400

    initial commit

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,29 @@
+import Particles from "react-tsparticles";
+import { Container } from "./styles";
+import { Hero } from "../Hero/Hero";
+import { About } from "../About/About";
+import { Contact } from "../Contact/Contact";
+import { Portfolio } from "../Portfolio/Portfolio";
+
+import wordpress from "../../assets/wordpress.svg";
+import shopify from "../../assets/shopify.svg";
+import htmlIcon from "../../assets/html-icon.svg";
+import cssIcon from "../../assets/css-icon.svg";
+import sassIcon from "../../assets/sass-icon.svg";
+import jsIcon from "../../assets/js-icon.svg";
+import nodeIcon from "../../assets/node-icon.svg";
+import reactIcon from "../../assets/react-icon.svg";
+import typescriptIcon from "../../assets/typescript-icon.svg";
+import vueIcon from "../../assets/vue-icon.svg";
+import boostrapIcon from "../../assets/bootstrap-icon.svg";
+
+export function Main() {
+  return (
+    <Container>
+      <Hero></Hero>
+      <About></About>
+      <Portfolio></Portfolio>
+      <Contact></Contact>
+    </Container>
+  );
+}
